ff9b9890568e7da60e7ff8cc21780ba272066e08
darkcat013 <viorel.noroc013@gmail.com>
2022-04-16 01:48:39 +0300

    fix some edge cases for ambiguity and add extra checks

--- lab5/lab5/Parser.cs
+++ lab5/lab5/Parser.cs
@@ -14,7 +14,7 @@
                 string right = grammar.Input[i].ToString();
                 if(string.IsNullOrEmpty(m.Matrix[left][right]))
                 {
-                    Console.WriteLine($"Could not parse Input at character: {right}");
+                    Console.WriteLine($"Could not parse Input at characters: {left}{right}");
                     return;
                 }
                 initialParsing += $"{left}{m.Matrix[left][right]}";
@@ -40,35 +40,61 @@
             var newDerivation = "";
             if(!keysWithDerivation.Any())
             {
-                Console.WriteLine("Could not parse Input");
+                Console.WriteLine("Could not parse Input (keys with derivations not found)");
                 return;
             }
             if(keysWithDerivation.Count == 1) newDerivation = keysWithDerivation[0];
             else
             {
                 var possibleDerivation = "";
+                var bestPickedDefinitions = "";
                 foreach (var key in keysWithDerivation)
                 {
                     var leftDefinition = m.Matrix[ParsingValue[posLeft - 1]][key];
                     var rightDefinition = m.Matrix[key][ParsingValue[posRight + 1]];
+                    var pickedDefinitions = leftDefinition + rightDefinition;
                     if(!string.IsNullOrEmpty(leftDefinition) && !string.IsNullOrEmpty(rightDefinition))
                     {
-                        if (string.IsNullOrEmpty(possibleDerivation)) possibleDerivation = key;
-                        else if(leftDefinition == "=" || rightDefinition == "=") possibleDerivation = key;
+                        if (leftDefinition == "=" && rightDefinition == "=") { possibleDerivation = key; bestPickedDefinitions = pickedDefinitions; }
+                        else if ((leftDefinition == "=" || rightDefinition == "=") && bestPickedDefinitions!="==") { possibleDerivation = key; bestPickedDefinitions = pickedDefinitions; }
+                        else if (!bestPickedDefinitions.Contains('=')){ possibleDerivation = key; bestPickedDefinitions = pickedDefinitions; }
+                        
                     }
                 }
                 newDerivation = possibleDerivation;
             }
             if(string.IsNullOrEmpty(newDerivation))
             {
-                Console.WriteLine("Could not parse Input");
+                Console.WriteLine("Could not parse Input (no new derivation)");
                 return;
             }
             ParsingValue.RemoveRange(posLeft + 1, posRight - posLeft - 1);
             ParsingValue.Insert(posLeft + 1, newDerivation);
-
-            ParsingValue[posLeft] = m.Matrix[ParsingValue[posLeft - 1]][ParsingValue[posLeft + 1]];
-            ParsingValue[posLeft+2] = m.Matrix[ParsingValue[posLeft + 1]][ParsingValue[posLeft + 3]];
+            var replaceLeft = m.Matrix[ParsingValue[posLeft - 1]][ParsingValue[posLeft + 1]];
+            var replaceRight = m.Matrix[ParsingValue[posLeft + 1]][ParsingValue[posLeft + 3]];
+            var testValue = ParsingValue.Select(x => x).ToList();
+            testValue[posLeft] = replaceLeft;
+            testValue[posLeft + 2] = replaceRight;
+            if (string.IsNullOrEmpty(replaceLeft) && string.IsNullOrEmpty(replaceRight))
+            {
+                Console.WriteLine(string.Join("", testValue));
+                Console.WriteLine($"Could not parse Input (no value in matrix[{ParsingValue[posLeft - 1]}][{ParsingValue[posLeft + 1]}] and matrix[{ParsingValue[posLeft + 1]}][{ParsingValue[posLeft + 3]}])");
+                return;
+            }
+            else if(string.IsNullOrEmpty(replaceLeft))
+            {
+                Console.WriteLine(string.Join("", testValue));
+                Console.WriteLine($"Could not parse Input (no value in matrix[{ParsingValue[posLeft - 1]}][{ParsingValue[posLeft + 1]}])");
+                return;
+            }
+            else if(string.IsNullOrEmpty(replaceRight))
+            {
+                Console.WriteLine(string.Join("", testValue));
+                Console.WriteLine($"Could not parse Input (no value in matrix[{ParsingValue[posLeft + 1]}][{ParsingValue[posLeft + 3]}])");
+                return;
+            }
+            ParsingValue[posLeft] = replaceLeft;
+            ParsingValue[posLeft+2] = replaceRight;
 
             Console.WriteLine(string.Join("", ParsingValue));
             Parse(grammar, m);
